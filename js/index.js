fb55739e9b42a38ae282030805c266e35727b9f9
Chris Zuber <shgysk8zer0@gmail.com>
2019-10-24 15:01:09 -0700

    Initial Setup
    Add icons and dummy content

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
 import 'https://cdn.kernvalley.us/components/share-button.js';
 import 'https://cdn.kernvalley.us/components/current-year.js';
+import 'https://cdn.kernvalley.us/components/bacon-ipsum.js';
+import 'https://cdn.kernvalley.us/components/toast-message.js';
 import {$, ready, registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
